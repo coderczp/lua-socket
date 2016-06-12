7562e97c08ebe0f45d714666653638c67af0f066
me <mephostopilis@gmail.com>
2016-06-12 21:46:20 +0800

    fix

--- lpackagesocket.c
+++ lpackagesocket.c
@@ -1,10 +1,20 @@
-// #include "PackageSocket.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdint.h>
-
+#include <assert.h>
 
 #include <lua.h>
 #include <lauxlib.h>
+
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
 
 static int 
 lconnect(lua_State *L) {
@@ -164,6 +174,107 @@ ltest3(lua_State *L) {
 	return 1;
 }
 
+struct packagesocketudp
+{
+	int fd;
+	struct sockaddr local;
+	struct sockaddr remote;
+	char buffer[1024];
+	int len;
+};
+
+static int
+ludp(lua_State *L) {
+	struct packagesocketudp *ps = malloc(sizeof(*ps));
+	ps->fd = 0;
+	memset(&ps->local, 0, sizeof(ps->local));
+	memset(&ps->remote, 0, sizeof(ps->remote));
+	memset(&ps->buffer, 0, 1024);
+	ps->len = 1024;
+
+	int t = luaL_checkinteger(L, 1);
+	if (t == 1) {
+		size_t sz = 0;
+		const char *laddr = luaL_checklstring(L, 2, &sz);
+		int lport = luaL_checkinteger(L, 3);	
+		struct sockaddr_in * local = (struct sockaddr_in *)&ps->local;	
+		(local)->sin_family = AF_INET;
+		(local)->sin_port = htons(lport);	
+		inet_pton(AF_INET, laddr, &local->sin_addr);	
+		
+		const char *raddr = luaL_checklstring(L, 4, &sz);
+		int rport = luaL_checkinteger(L, 5);
+
+		struct sockaddr_in * remote = (struct sockaddr_in *)&ps->remote;
+		(remote)->sin_family = AF_INET;
+		(remote)->sin_port = htons(rport);
+		inet_pton(AF_INET, laddr, &remote->sin_addr);
+
+		int fd = socket(PF_INET, SOCK_DGRAM, 0);
+		ps->fd = fd;
+	} else if (t == 2) {
+		size_t sz = 0;
+		const char *laddr = luaL_checklstring(L, 2, &sz);
+		int lport = luaL_checkinteger(L, 3);
+		struct sockaddr_in6 *local = (struct sockaddr_in6 *)&ps->local;
+		local->sin_family = AF_INET6;
+		local->sin_port = htons(lport);
+		inet_pton(AF_INET6, laddr, &local->sin6_addr)
+		
+	}
+	if strlen(laddr) 
+
+	
+
+	
+
+	
+	
+	
+
+	
+	int r = bind(fd, &ps->local, sizeof(ps->local));
+
+	lua_pushlightuserdata(L, (void*)ps);
+	return 1;
+}
+
+static int
+ludp_connect(lua_State *L) {
+	struct packagesocketudp *ps  = (struct packagesocketudp *)lua_touserdata(L, 1);
+}
+
+static int
+ludp_send(lua_State *L) {
+	struct packagesocketudp *ps  = (struct packagesocketudp *)lua_touserdata(L, 1);
+	size_t sz = 0;
+	const char * buffer = luaL_checklstring(L, 2, &sz);
+	sendto(ps->fd, buffer, sz, 0, &ps->remote, sizeof(ps->remote));
+	return 0;
+}
+
+static int
+ludp_poll(lua_State *L) {
+	struct packagesocketudp *ps  = (struct packagesocketudp *)lua_touserdata(L, 1);
+	int r = recvfrom(ps->fd, ps->buffer, 256, 0, &ps->remote, sizeof(ps->remote));
+	if (r == -1) {
+		if (errno == EAGAIN || errno == EINTR) {
+		}
+		lua_pushinteger(L, r);
+		return 1;
+	} else {
+		lua_pushstring(L, ps->buffer);
+		lua_pushinteger(L, r);
+		return 2;
+	}
+}
+
+ludp_close(lua_State *L) {
+	struct packagesocketudp *ps  = (struct packagesocketudp *)lua_touserdata(L, 1);
+	close(ps->fd);
+	free(ps);
+}
+
 int
 luaopen_packagesocket(lua_State *L) {
 	luaL_Reg l[] = {
@@ -174,6 +285,10 @@ luaopen_packagesocket(lua_State *L) {
 		{ "test1", ltest1 },
 		{ "test2", ltest2 },
 		{ "test3", ltest3 },
+		{ "udp", ludp},
+		{ "udp_send", ludp_send},
+		{ "udp_poll", ludp_poll},
+		{ "udp_close", ludp_close},
 		{ NULL, NULL },
 	};
 	luaL_newlib(L,l);
